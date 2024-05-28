2f588976b4af0cac1920c137cd8d0901b201f330
playgoj <137984904+playgoj@users.noreply.github.com>
2024-05-28 20:01:03 +0200

    Setup Groq

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-
+from groq import Groq
+import os
+from dotenv import load_dotenv
+import pyperclip
 
 if __name__ == '__main__':
-    print('Starting Script....')
+    load_dotenv()
+    client = Groq(
+        api_key=os.getenv("GROQ_API_KEY"),
+    )
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": pyperclip.paste(),
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    pyperclip.copy(chat_completion.choices[0].message.content)
+
+
+
